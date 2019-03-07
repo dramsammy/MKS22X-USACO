6fd2458f3d8e51eabc46f2b5339981f35f79b8ab
dramsammy <dramsammy@stuy.edu>
2019-03-07 10:49:31 -0500

    splitting array

---
+++ USACO.java
@@ -0,0 +1,19 @@
+public class USACO{
+  public static ary[][] = new
+  public static int bronze(String filename) throws FileNotFoundException{
+    int check = arg1.length();
+    File file = new File(filename);
+    Scanner in = new Scanner(file);
+    String arg1 = in.nextLine();
+    int check = arg1.length();
+    while (in.hasNextLine()){
+      int temp = in.nextLine.length();
+      String[] ary = in.nextLine().split("\\s");
+    }
+
+  }
+  public static boolean stomp(int row, int col, int depth){
+
+  }
+
+}
